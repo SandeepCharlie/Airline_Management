1e66bc2f82c6731429dbb0241a5b1345e92e0101
iamsandeep <sandeepcharlie.2004@gmail.com>
2024-06-15 20:05:14 +0530

    Final changes by Sandeep

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,148 @@
-import LoginRegister from './Components/LoginRegister/LoginRegister';
+import React, { Fragment } from "react";
+import "./App.css";
+import CreateFlight from "./pages/CreateFlight/CreateFlight.jsx";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import DisplayFlights from "./pages/DisplayFlights/DisplayFlights.jsx";
+import Login from "./pages/Login/Login.jsx";
+import EditFlight from "./pages/EditFlight/EditFlight.jsx";
+import EditUserProfile from "./pages/EditUserProfile/EditUserProfile";
+import DisplayReservations from "./pages/DisplayReservations/DisplayReservations";
+import ChooseSeats from "./pages/ChooseSeats/ChooseSeats";
+import ShowReservation from "./pages/ShowReservation/ShowReservation";
+import Home from "./pages/Home/Home";
+import Search from "./pages/Search/Search";
+import FlightDetails from "./pages/FlightDetails/FlightDetails";
+import ShowSummary from "./pages/ShowSummary/ShowSummary";
+import SignUp from "./pages/Signup/Signup";
+import { useAuthContext } from "./Context";
+import NavBar from "./components/NavBar";
+import ProtectedRouteUser from "./routes/ProtectedRouteUser";
+import ProtectedRouteAdmin from "./routes/ProtectedRouteAdmin";
+import ReserveFlight from "./pages/ReserveFlight/ReserveFlight";
+import Cancel from "./pages/Cancel/Cancel";
+import ChangeSeats from './pages/ChangeSeats/ChangeSeats'
+import ReplaceFlight from './pages/ReplaceFlight/ReplaceFlight'
+import ChangePassword from "./pages/ChangePassword/ChangePassword";
+import FlightList from './pages/FlightList/FlightList'
+import NewFlightItenerary from './pages/NewFlightItenerary/NewFlightItenerary'
+import ChooseNewSeats from './pages/ChooseNewSeats/ChooseNewSeats'
 
 function App() {
+  const { isLoggedIn } = useAuthContext();
+
   return (
-    <div >
-        <LoginRegister />
+    <div className="App">
+      <BrowserRouter>
+
+        <NavBar />
+        <Routes>
+
+          <Route exact path="/" element={<Login />} />
+
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route
+            path="/home"
+            element={<Home />}
+          />
+
+          <Route exact path="/user/chooseNewSeats" element={<ProtectedRouteUser />}>
+            <Route exact path="/user/chooseNewSeats" element={<ChooseNewSeats />} />
+          </Route>
+
+          <Route exact path="/user/newFlightItenerary" element={<ProtectedRouteUser />}>
+            <Route exact path="/user/newFlightItenerary" element={<NewFlightItenerary />} />
+          </Route>
+
+          <Route exact path="/user/flightList" element={<ProtectedRouteUser />}>
+            <Route
+              exact
+              path="/user/flightList"
+              element={<FlightList />}
+            />
+          </Route>
+
+          <Route exact path="/home/search" element={<Search />} />
+          <Route
+            exact
+            path="/home/search/flightDetails"
+            element={<FlightDetails />}
+          />
+          <Route exact path="/flights" element={<ProtectedRouteAdmin />}>
+            <Route exact path="/flights" element={<DisplayFlights />} />
+          </Route>
+
+          <Route exact path="/user/replaceFlight/:id" element={<ProtectedRouteUser />}>
+            <Route exact path="/user/replaceFlight/:id" element={<ReplaceFlight />} />
+          </Route>
+
+          <Route exact path="/user/changePassword" element={<ProtectedRouteUser />}>
+            <Route exact path="/user/changePassword" element={<ChangePassword />} />
+          </Route>
+
+          <Route exact path="/flights/create" element={<ProtectedRouteAdmin />}>
+            <Route
+              exact
+              path="/flights/create"
+              element={<CreateFlight />}
+            />
+          </Route>
+
+          <Route exact path="/flight/edit" element={<ProtectedRouteAdmin />}>
+            <Route exact path="/flight/edit" element={<EditFlight />} />
+          </Route>
+
+          <Route exact path="/user/reserveFlight" element={<ProtectedRouteUser />}>
+            <Route exact path="/user/reserveFlight" element={<ReserveFlight />} />
+          </Route>
+
+          <Route exact path="/user/cancel" element={<ProtectedRouteUser />}>
+            <Route exact path="/user/cancel" element={<Cancel />} />
+          </Route>
+
+          <Route exact path="/user/edit" element={<ProtectedRouteUser />}>
+            <Route exact path="/user/edit" element={<EditUserProfile />} />
+          </Route >
+
+          <Route exact path="/user/changeSeats/:id" element={<ProtectedRouteUser />}>
+            <Route exact path="/user/changeSeats/:id" element={<ChangeSeats />} />
+          </Route >
+
+          <Route exact path="/user/reservations" element={<ProtectedRouteUser />}>
+            <Route
+              exact
+              path="/user/reservations"
+              element={<DisplayReservations />}
+            />
+          </Route>
+
+          <Route exact path="/user/showReservation/:id" element={<ProtectedRouteUser />}>
+            <Route
+              exact
+              path="/user/showReservation/:id"
+              element={<ShowReservation />}
+            />
+          </Route>
+
+          <Route
+            exact
+            path="/user/chooseSeats"
+            element={<ChooseSeats />}
+          />
+          <Route
+            exact
+            path="/reservation/summary"
+            element={<ShowSummary />}
+          />
+          {/* <Route path="*" element={<Navigate to="/home" />} /> */}
+        </Routes>
+
+      </BrowserRouter>
+
+
     </div>
   );
 }
---
+++ frontend/src/Context.js
@@ -0,0 +1,129 @@
+import React, { useContext, createContext, useState, useEffect } from "react";
+import {
+  NotificationManager,
+  NotificationContainer,
+} from "react-notifications";
+
+
+export const UserContext = createContext(null);
+
+// const AppContext = createContext(null);
+const AuthContext = createContext(null);
+
+export const useAppContext = () => useContext(UserContext);
+export const useAuthContext = () => useContext(AuthContext);
+
+// This context provider is passed to any component requiring the context
+export const UserProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    // Cookies.get("accessToken") ? true : false
+    localStorage.getItem('token') ? true : false
+  );
+
+  const [userData, setUserData] = useState(
+    // Cookies.get("userData") ? JSON.parse(Cookies.get("userData")) : {}
+    localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')) : {}
+
+  );
+  const createNotification = (message = "", type = "error") => {
+    switch (type) {
+      case "info":
+        NotificationManager.info(message);
+        break;
+      case "success":
+        NotificationManager.success(message);
+        break;
+      case "warning":
+        NotificationManager.warning(message);
+        break;
+      case "error":
+        NotificationManager.error(message);
+        break;
+    }
+  };
+
+  useEffect(() => {
+    if (!localStorage.getItem('token')) {
+      setIsLoggedIn(false);
+    } else {
+      setUserData(JSON.parse(localStorage.getItem('userData')));
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      // Cookies.remove("accessToken");
+      // Cookies.remove("userData");
+      // localStorage.removeItem('token');
+      // localStorage.removeItem('userData');
+
+    } else {
+      setUserData(
+        // Cookies.get("userData") ? JSON.parse(Cookies.get("userData")) : {}
+        localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')) : {}
+      );
+    }
+  }, [isLoggedIn]);
+
+  const [searchCriteria, setSearchCriteria] = useState(null);
+  const [departureFlights, setDepartureFlights] = useState(null);
+  const [returnFlights, setReturnFlights] = useState(null);
+  const [chosenDepartureFlight, setChosenDepartureFlight] = useState(null);
+  const [chosenReturnFlight, setChosenReturnFlight] = useState(null);
+  const [reservation, setReservation] = useState(null);
+  const [chosenSeatsDeparture, setChosenSeatsDeparture] = useState([]);
+  const [chosenSeatsReturn, setChosenSeatsReturn] = useState([]);
+  const [flightType, setFlightType] = useState("");
+  const [cabinClass, setCabinClass] = useState("");
+  const [oldDepFlight, setOldDepFlight] = useState(null);
+  const [flightsResults, setFlightsResults] = useState([]);
+
+  const [priceDifferences, setPriceDifferences] = useState([]);
+  const [newFlight, setNewFlight] = useState(null);
+  const [newCabinClass, setNewCabinClass] = useState(null);
+
+  return (
+    <UserContext.Provider
+      value={{
+        searchCriteria,
+        setSearchCriteria,
+        departureFlights,
+        setDepartureFlights,
+        returnFlights,
+        setReturnFlights,
+        chosenDepartureFlight,
+        setChosenDepartureFlight,
+        chosenReturnFlight,
+        setChosenReturnFlight,
+        reservation,
+        setReservation,
+        chosenSeatsDeparture,
+        setChosenSeatsDeparture,
+        createNotification,
+        userData,
+        chosenSeatsReturn,
+        setChosenSeatsReturn,
+        flightType,
+        setFlightType,
+        cabinClass,
+        setCabinClass,
+        oldDepFlight,
+        setOldDepFlight,
+        flightsResults,
+        setFlightsResults,
+        priceDifferences,
+        setPriceDifferences,
+        newFlight,
+        setNewFlight,
+        newCabinClass,
+        setNewCabinClass
+      }}
+    >
+      <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+        <NotificationContainer />
+        {children}
+      </AuthContext.Provider>
+    </UserContext.Provider>
+  );
+};
